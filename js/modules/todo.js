af7a2f4f155939df3b121ed73e18f3eb879a59c7
ohuton55 <tttt.tttt.no@gmail.com>
2023-08-29 09:42:35 +0900

    push inputData for localStorage.

--- js/modules/todo.js
+++ js/modules/todo.js
@@ -1,6 +1,26 @@
 export function hello() {
-    console.log('Hello from todo.');
+  const addForm = document.querySelector('.td-add-form');
+  const addInput = document.querySelector('.td-add-input');
+
+  let todoData = [];
+
+  addForm.addEventListener('submit', e => {
+    e.preventDefault(); // デフォルトの更新を抑制する
+
+    let todoObj = {
+      content: addInput.value.trim(),
+      isDone: false
+   };
+  
+  todoData.push(todoObj);
+  addInput.value = '';
+
+  updateLS(todoData);
+
+  })
 }
 
-  console.log('test from todo.');
-
+export function updateLS(data){
+    // localStorageへtodoDataをJSONに変換してからセット
+    localStorage.setItem('myTodo', JSON.stringify(data));
+}
