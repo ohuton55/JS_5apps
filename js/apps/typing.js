864443cc0010561a9ed5b427f06fb8c0e5a08d20
ohuton55 <tttt.tttt.no@gmail.com>
2023-10-29 17:49:24 +0900

    Enable the game to start under specific conditions

--- js/apps/typing.js
+++ js/apps/typing.js
@@ -5,19 +5,28 @@ const typingGame = document.querySelector('#ty-game');
 const titleTime = document.querySelector('#ty-title-time');
 const timer = document.querySelector('#ty-timer');
 const timeSelectEl = document.querySelector('.ty-time-select');
+const typing = document.querySelector('#typing');
+const backToStart = document.querySelector('#back-to-start');
+const resultContainer = document.querySelector('#ty-result-container');
 
 let timelimit = 30; // 制限時間
 let remainingTime;  // 残り時間
+let isActive = false;   // タイピングメニューがアクティブか
+let isPlaying = false;  // タイピングゲームをプレイ中か
 
 timeSelectEl.addEventListener('change', () => {
     timelimit = timeSelectEl.value;
 })
+
 window.addEventListener('keypress', e => {
+    isActive = typing.classList.contains('active');
+
     if(e.key === 'Enter'){
-        console.log('Enter');
+        // Enterが押され、typing sectionがactiveのときゲームをスタート
         start();
+        isActive = false;
+        isPlaying = true;
     }
-    console.log(e.key);
     return;
 })
 
@@ -28,3 +37,11 @@ function start(){
     remainingTime = timelimit;
     timer.textContent = remainingTime;
 }
+
+// スタートに戻るボタンを押された
+backToStart.addEventListener('click', () => {
+    isPlaying = false;
+    typingGame.classList.remove('show');
+    startPage.classList.add('show');
+    resultContainer.classList.remove('show');
+})
