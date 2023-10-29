6b299a9c68228fc934539a8f25ee4b3be3758571
ohuton55 <tttt.tttt.no@gmail.com>
2023-10-29 19:07:35 +0900

    Adjust countdown and screen transition timing.

--- js/apps/typing.js
+++ js/apps/typing.js
@@ -6,13 +6,15 @@ const titleTime = document.querySelector('#ty-title-time');
 const timer = document.querySelector('#ty-timer');
 const timeSelectEl = document.querySelector('.ty-time-select');
 const typing = document.querySelector('#typing');
-const backToStart = document.querySelector('#back-to-start');
+const backToStart = document.querySelector('#ty-back-to-start');
 const resultContainer = document.querySelector('#ty-result-container');
+const textarea = document.querySelector('#ty-textarea');
 
 let timelimit = 30; // 制限時間
 let remainingTime;  // 残り時間
 let isActive = false;   // タイピングメニューがアクティブか
 let isPlaying = false;  // タイピングゲームをプレイ中か
+let intervalId = null;
 
 timeSelectEl.addEventListener('change', () => {
     timelimit = timeSelectEl.value;
@@ -21,11 +23,13 @@ timeSelectEl.addEventListener('change', () => {
 window.addEventListener('keypress', e => {
     isActive = typing.classList.contains('active');
 
-    if(e.key === 'Enter'){
-        // Enterが押され、typing sectionがactiveのときゲームをスタート
-        start();
-        isActive = false;
-        isPlaying = true;
+    if(e.key === 'Enter' && isActive){
+        if(!isPlaying){
+            isActive = false;
+            isPlaying = true;
+            // Enterが押され、typing sectionがactiveのときゲームをスタート
+            start();
+        }   
     }
     return;
 })
@@ -36,6 +40,16 @@ function start(){
     titleTime.textContent = timelimit;
     remainingTime = timelimit;
     timer.textContent = remainingTime;
+    textarea.focus();
+    textarea.disabled = false;  // テキストアリア入力有効
+
+    intervalId = setInterval(() => {
+        remainingTime -= 1;
+        timer.textContent = remainingTime;
+        if(remainingTime <= 0){
+            showResult();
+        }
+    }, 1000);
 }
 
 // スタートに戻るボタンを押された
@@ -45,3 +59,11 @@ backToStart.addEventListener('click', () => {
     startPage.classList.add('show');
     resultContainer.classList.remove('show');
 })
+
+function showResult(){
+    textarea.disabled = true;   // テキストアリア入力無効
+    clearInterval(intervalId);  // 引数のsetInterval()をクリアする
+    setInterval(() => {
+        resultContainer.classList.add('show');
+    }, 1000);
+}
