d806e7d5b6d452ae8e2256bce02bb5311dc0a5aa
ohuton55 <tttt.tttt.no@gmail.com>
2023-10-29 17:33:17 +0900

    add event listeners and game start func.

--- js/apps/typing.js
+++ js/apps/typing.js
@@ -2,12 +2,29 @@ console.log('hello from modules');
 
 const startPage = document.querySelector('#ty-start-page');
 const typingGame = document.querySelector('#ty-game');
-const titleTime = document.querySelector('#ty-title');
+const titleTime = document.querySelector('#ty-title-time');
 const timer = document.querySelector('#ty-timer');
+const timeSelectEl = document.querySelector('.ty-time-select');
 
 let timelimit = 30; // 制限時間
 let remainingTime;  // 残り時間
 
-window.addEventListener('keypress', e => {
-    console.log(e);
+timeSelectEl.addEventListener('change', () => {
+    timelimit = timeSelectEl.value;
 })
+window.addEventListener('keypress', e => {
+    if(e.key === 'Enter'){
+        console.log('Enter');
+        start();
+    }
+    console.log(e.key);
+    return;
+})
+
+function start(){
+    startPage.classList.remove('show');
+    typingGame.classList.add('show');
+    titleTime.textContent = timelimit;
+    remainingTime = timelimit;
+    timer.textContent = remainingTime;
+}
