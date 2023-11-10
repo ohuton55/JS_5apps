189ef3dc9560860587c6610c0ffed8036dc5eb13
ohuton55 <tttt.tttt.no@gmail.com>
2023-11-10 10:20:15 +0900

    create fnc for empty grid

--- js/apps/life.js
+++ js/apps/life.js
@@ -7,10 +7,14 @@ const canvasContainer = document.querySelector('.gl-canvas-container');
 const pattern = document.querySelector('.gl-pattern');
 const controller = document.querySelector('.gl-controller');
 const backToMenu = document.querySelector('.gl-back-to-menu');
+const canvas = document.querySelector('#canvas');
 
 let size = sizeSelect.value;
 let speed = speedSelect.value;
 let runningState = false;
+let grid;
+let numOfCols;
+let numOfRows;
 const RESUME_COLOR = `#0aa`;
 const PAUSE_COLOR = `#f55`;
 const ALIVE_CELL = `#0f7`;
@@ -30,6 +34,8 @@ randomBtns.forEach(btn => {
         runningState = true;
         controller.textContent = `Pause`;
         controller.style.backgroundColor = PAUSE_COLOR;
+        grid = createRandomGrid(btn.dataset.value);
+        drawAliveCells(grid);
     })
 })
 selfSelectBtn.addEventListener('click', () => {
@@ -57,3 +63,34 @@ controller.addEventListener('click', () => {
         controller.style.backgroundColor = PAUSE_COLOR;
     }
 })
+
+function generateEmptyGrid(){
+    // width/height は size で割り切れる固定値にしている
+    numOfCols = canvas.width / size;
+    numOfRows = canvas.height / size;
+    console.log(numOfCols);
+    console.log(numOfRows);
+    let emptyGrid = new Array(numOfCols);
+
+    for(let i = 0; i < emptyGrid.length; i++){
+        emptyGrid[i] = new Array(numOfRows);
+    }
+    
+    return emptyGrid
+}
+
+function createRandomGrid(num){
+    grid = generateEmptyGrid();
+    for (let i = 0; i < numOfCols; i++){
+        for (let j = 0; j < numOfRows; j++){
+            grid[i][j] = Math.floor(Math.random() * num);
+            // grid[i][j] = Math.floor(Math.random() * 2);
+            // Math.random() * 2 ... およそ50%の確率で0と1が出る
+        }
+    }
+    return grid
+}
+
+function drawAliveCells(grid){
+
+}
